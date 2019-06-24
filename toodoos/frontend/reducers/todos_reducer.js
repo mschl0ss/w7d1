8d60b68ff1d2bb7d80dbabfb89ddb076c49eaa73
mschl0ss <mr.schlossberg@gmail.com>
2019-06-24 14:51:31 -0700

    Adding root reducer functionality

---
+++ toodoos/frontend/reducers/todos_reducer.js
@@ -0,0 +1,12 @@
+import {RECEIVE_TODOS, RECEIVE_TODO} from '../actions/todos_actions'
+
+const todosReducer = (state = {}, action)=>{
+  switch(action.type){
+    
+    default:
+      return state;
+  }
+};
+
+
+export default todosReducer;
